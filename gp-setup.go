79be6dfe4665932a41ad7e7d4d508a6f59f8e32f
Sean Hellum <seanhellum45@gmail.com>
2019-12-20 21:55:25 +0000

    put back

--- gp-setup.go
+++ gp-setup.go
@@ -97,3 +97,34 @@ Language:
 		}
 	}
 }
+func juliaInit() {
+	initBase(juliaDockerFile, juliaYaml)
+}
+func nimInit() {
+	initBase(nimDockerFile, nimYaml)
+}
+func hyInit() {
+	initBase(hyDockerfile, hyYaml)
+}
+func clojureInit() {
+	initBase(clojureDockerfile, clojureYaml)
+}
+func haskellInit() {
+	initBase(haskellDockerfile, haskellYaml)
+}
+func dotNetInit() {
+	initBase(dotNetDockerfile, dotNetYaml)
+}
+func zshInit() {
+	initBase(zshDockerfile, zshYaml)
+}
+func initBase(dockerFile, Yaml string) {
+	gitpodDockerfile, _ := os.Create(".gitpod.Dockerfile")
+	gitpodYaml, _ := os.Create(".gitpod.yml")
+	gitpodDockerfile.WriteString(dockerFile)
+	gitpodYaml.WriteString(Yaml)
+}
+func exit() {
+	fmt.Println("Ok, bye!")
+	os.Exit(0)
+}
