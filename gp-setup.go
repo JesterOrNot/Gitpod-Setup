2aae58480af065fe0141e07394a71fd240892b77
Sean Hellum <seanhellum45@gmail.com>
2019-12-22 13:59:56 -0600

    Update gp-setup.go

--- gp-setup.go
+++ gp-setup.go
@@ -298,22 +298,18 @@ vscode:
       - dramforever.vscode-ghc-simple@0.1.13:X3A6Dr3LYAP8MxXBh/hb1A==
 `
 var dotNetDockerfile string = `FROM gitpod/workspace-full
-
 USER gitpod
-
-# Install F#
-RUN sudo apt-get update \
-	&& sudo apt-get install -y \
-		fsharp
 
 # Install .NET
 RUN wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb \
 	&& sudo dpkg -i packages-microsoft-prod.deb \
 	&& sudo apt-get update \
-	&& sudo apt-get update \
 	&& sudo apt-get install -y \
+        fsharp \
 		apt-transport-https \
-		dotnet-sdk-3.0
+		dotnet-sdk-3.1 \
+        aspnetcore-runtime-3.1 \
+        dotnet-runtime-3.1
 `
 var dotNetYaml string = `image:
   file: .gitpod.Dockerfile
